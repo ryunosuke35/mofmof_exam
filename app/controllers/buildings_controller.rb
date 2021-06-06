bacf93e3a695e96f3ee947b72b77531416b19dc2
ryunosuke35 <6bev2103@gmail.com>
2021-06-06 17:48:53 +0900

    viewの作成

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -22,39 +22,27 @@ class BuildingsController < ApplicationController
   # POST /buildings or /buildings.json
   def create
     @building = Building.new(building_params)
-
-    respond_to do |format|
       if @building.save
-        format.html { redirect_to @building, notice: "Building was successfully created." }
-        format.json { render :show, status: :created, location: @building }
+        redirect_to @building, notice: "Building was successfully created."
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @building.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
     end
-  end
 
   # PATCH/PUT /buildings/1 or /buildings/1.json
   def update
-    respond_to do |format|
       if @building.update(building_params)
-        format.html { redirect_to @building, notice: "Building was successfully updated." }
-        format.json { render :show, status: :ok, location: @building }
+        redirect_to @building, notice: "Building was successfully updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @building.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
     end
-  end
 
   # DELETE /buildings/1 or /buildings/1.json
   def destroy
     @building.destroy
-    respond_to do |format|
-      format.html { redirect_to buildings_url, notice: "Building was successfully destroyed." }
-      format.json { head :no_content }
+      redirect_to buildings_url, notice: "Building was successfully destroyed."
     end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -64,6 +52,6 @@ class BuildingsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def building_params
-      params.fetch(:building, {})
+      params.require(:building).permit(:name, :rent, :address, :age_of_building, :note)
     end
 end
