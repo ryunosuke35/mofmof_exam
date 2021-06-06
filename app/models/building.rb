0c234c7e3aa0d58f005109da2a9f0610532e7a41
ryunosuke35 <6bev2103@gmail.com>
2021-06-06 17:57:36 +0900

    バリデーションの設定

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,6 @@
 class Building < ApplicationRecord
-end
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :rent, numericality: { only_integer: true }
+  validates :address, presence: true, length: { maximum: 100 }
+  validates :age_of_building, numericality: { only_integer: true }
+  end
