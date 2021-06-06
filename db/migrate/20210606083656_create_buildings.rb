03b2c644e42693f79e359c8548a781e180bc9d05
ryunosuke35 <6bev2103@gmail.com>
2021-06-06 17:53:32 +0900

    buildingsテーブルの作成

--- db/migrate/20210606083656_create_buildings.rb
+++ db/migrate/20210606083656_create_buildings.rb
@@ -1,7 +1,11 @@
 class CreateBuildings < ActiveRecord::Migration[5.2]
   def change
     create_table :buildings do |t|
-
+      t.string "name"
+      t.integer "rent"
+      t.string "address"
+      t.integer "age_of_building"
+      t.text "note"
       t.timestamps
     end
   end
