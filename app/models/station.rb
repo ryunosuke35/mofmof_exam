be14badb2bc929e43011a606c8d0816b90f87883
ryunosuke35 <6bev2103@gmail.com>
2021-06-08 16:33:03 +0900

    最寄駅のバリデーションを削除

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,7 +1,3 @@
 class Station < ApplicationRecord
-  validates :toure_name, presence: true, length: { maximum: 50 }
-  validates :station_name, presence: true, length: { maximum: 50 }
-  validates :minutes_of_foot, presence: true, length: { maximum: 10 }
-
-    belongs_to :building
+  belongs_to :building
 end
