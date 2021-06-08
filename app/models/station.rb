befa910d2e69a98ff5c34ee7cf8bc77ef6b6ec46
ryunosuke35 <6bev2103@gmail.com>
2021-06-08 16:31:36 +0900

    最寄駅のモデルにバリデーションを設定

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,7 @@
 class Station < ApplicationRecord
+  validates :toure_name, presence: true, length: { maximum: 50 }
+  validates :station_name, presence: true, length: { maximum: 50 }
+  validates :minutes_of_foot, presence: true, length: { maximum: 10 }
+
     belongs_to :building
 end
